b97b162cd59017eab9e966d43b719392222a9275
Kaywuyep <wuyepkate@gmail.com>
2024-05-28 12:55:27 +0000

    task

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""A simple Flask app with Babel for i18n"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    """Return a simple template"""
+    return render_template("1-index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=3000)
