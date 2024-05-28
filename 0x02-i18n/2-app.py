fb79775356071cd98daa11ef855d28132a8f6e89
Kaywuyep <wuyepkate@gmail.com>
2024-05-28 13:01:14 +0000

    task

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -6,6 +6,9 @@ from flask_babel import Babel
 
 
 class Config:
+    """
+    Represents a Flask Babel configuration.
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
